64fbcfedcab3f28f51a737857d3414aaf097ed0e
cauetrainotti <trainotti.caue@gmail.com>
2025-12-03 20:31:23 -0300

    Infraestrutura das migrações do EF

---
+++ BuddyTech.API/Infra/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BuddyTech.API.Models;
+
+namespace BuddyTech.API.Infra
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Lead> Leads { get; set; }
+        public DbSet<LeadInteraction> LeadInteractions { get; set; }
+        public DbSet<LeadScore> LeadScores { get; set; }
+        public DbSet<Mission> Missions { get; set; }
+        public DbSet<Seller> Sellers { get; set; }
+        public DbSet<SellerMission> SellerMissions { get; set; }
+        public DbSet<Suggestion> Suggestions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BuddyTech.API/Infra/ApplicationDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace BuddyTech.API.Infra
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            builder.UseNpgsql(connectionString);
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}

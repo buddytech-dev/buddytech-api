69bb1fe43f25d06bd190f2c0aa38df15a3b8dcdb
cauetrainotti <trainotti.caue@gmail.com>
2025-12-04 21:21:23 -0300

    Criação de DTOs para SellerController

---
+++ BuddyTech.API/DTOs/SellerResponseDto.cs
@@ -0,0 +1,16 @@
+﻿namespace BuddyTech.API.DTOs
+    {
+        public class SellerResponseDto
+        {
+            public Guid SellerId { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Role { get; set; }
+            public string PhoneNumber { get; set; }
+            public int TotalPoints { get; set; }
+            public int MissionsCompletedCount { get; set; }
+            public int TotalLeadsAssigned { get; set; }
+            public int LeadsWonCount { get; set; }
+            public double ClosingRate { get; set; }
+        }
+    }

a7ace0911636f8c8c22ebf5df77f96cd58d4c870
cauetrainotti <trainotti.caue@gmail.com>
2025-12-04 21:22:08 -0300

    Migrations do banco

---
+++ BuddyTech.API/Migrations/20251204142838_AddCurrentPointsToSeller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BuddyTech.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCurrentPointsToSeller : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CurrentPoints",
+                table: "Sellers",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CurrentPoints",
+                table: "Sellers");
+        }
+    }
+}
